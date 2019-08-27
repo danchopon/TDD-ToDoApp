70bb388a871ec0c320b9e3bb955dcf619c3868ed
danchopon <dan4111k1@gmail.com>
2019-08-27 17:45:30 +0600

    finish Location model and it's tests

--- ToDoApp/Model/Location.swift
+++ ToDoApp/Model/Location.swift
@@ -7,7 +7,23 @@
 //
 
 import Foundation
+import CoreLocation
 
-struct Location: Equatable {
+struct Location {
     let name: String
+    let coordinate: CLLocationCoordinate2D?
+    
+    init(name: String, coordinate: CLLocationCoordinate2D? = nil) {
+        self.name = name
+        self.coordinate = coordinate
+    }
+}
+
+extension Location: Equatable {
+    static func == (lhs: Location, rhs: Location) -> Bool {
+        guard lhs.coordinate?.latitude == rhs.coordinate?.latitude &&
+            lhs.coordinate?.longitude == rhs.coordinate?.longitude &&
+            lhs.name == rhs.name else { return false }
+        return true
+    }
 }
--- ToDoAppTests/Model/LocationTests.swift
+++ ToDoAppTests/Model/LocationTests.swift
@@ -7,7 +7,23 @@
 //
 
 import XCTest
+import CoreLocation
 @testable import ToDoApp
 
 class LocationTests: XCTestCase {
+    
+    func testInitSetsName() {
+        let location = Location(name: "Foo")
+        
+        XCTAssertEqual(location.name, "Foo")
+    }
+    
+    func testInitSetCoordinates() {
+        let coordinate = CLLocationCoordinate2D(latitude: 1, longitude: 2)
+        
+        let location = Location(name: "Foo", coordinate: coordinate)
+        XCTAssertEqual(location.coordinate?.latitude, coordinate.latitude)
+        XCTAssertEqual(location.coordinate?.longitude, coordinate.longitude)
+    }
+    
 }

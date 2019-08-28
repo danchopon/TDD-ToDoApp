0a359043ee3af789f091c4a5c94641b97d3748b3
danchopon <dan4111k1@gmail.com>
2019-08-28 16:23:28 +0600

    finish task cell tests

--- ToDoApp/View/TaskCell.swift
+++ ToDoApp/View/TaskCell.swift
@@ -13,7 +13,32 @@ class TaskCell: UITableViewCell {
         return String(describing: self)
     }
     
-    func configure(withTask task: Task) {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    private var dateFormatter: DateFormatter = {
+        let df = DateFormatter()
+        df.dateFormat = "dd.MM.yy"
+        return df
+    }()
+    
+    func configure(withTask task: Task, done: Bool = false) {
+        
+        if done {
+            let attributedString = NSAttributedString(string: task.title, attributes: [NSAttributedString.Key.strikethroughStyle : NSUnderlineStyle.single.rawValue])
+            titleLabel.attributedText = attributedString
+            dateLabel = nil
+            locationLabel = nil
+        } else {
+            if let date = task.date {
+                let dateString = dateFormatter.string(from: date)
+                self.dateLabel.text = dateString
+            }
+            
+            self.titleLabel.text = task.title
+            self.locationLabel.text = task.location?.name
+        }
         
     }
 }
---
+++ ToDoAppTests/View/TaskCellTests.swift
@@ -0,0 +1,122 @@
+//
+//  TaskCellTests.swift
+//  ToDoAppTests
+//
+//  Created by Daniyar Erkinov on 8/28/19.
+//  Copyright © 2019 danchopon.com. All rights reserved.
+//
+
+import XCTest
+@testable import ToDoApp
+
+class TaskCellTests: XCTestCase {
+    
+    var cell: TaskCell!
+
+    override func setUp() {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let controller = storyboard.instantiateViewController(withIdentifier: String(describing: TaskListViewController.self)) as! TaskListViewController
+        controller.loadViewIfNeeded()
+        
+        let tableView = controller.tableView
+        let dataSource = FakeDataSource()
+        tableView?.dataSource = dataSource
+        
+        cell = tableView?.dequeueReusableCell(withIdentifier: String(describing: TaskCell.self), for: IndexPath(row: 0, section: 0)) as? TaskCell
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    func testCellHasTitleLabel() {
+        XCTAssertNotNil(cell.titleLabel)
+    }
+    
+    func testCellHasTitleLabelInContentView() {
+        XCTAssertTrue(cell.titleLabel.isDescendant(of: cell.contentView))
+    }
+    
+    func testCellHasLocationLabel() {
+        XCTAssertNotNil(cell.locationLabel)
+    }
+    
+    func testCellHasLocationLabelInContentView() {
+        XCTAssertTrue(cell.titleLabel.isDescendant(of: cell.contentView))
+    }
+    
+    func testCellHasDateLabel() {
+        XCTAssertNotNil(cell.dateLabel)
+    }
+    
+    func testCellHasDateLabelInContentView() {
+        XCTAssertTrue(cell.titleLabel.isDescendant(of: cell.contentView))
+    }
+    
+    func testConfigureSetsTitle() {
+        let task = Task(title: "Foo")
+        cell.configure(withTask: task)
+        
+        XCTAssertEqual(cell.titleLabel.text, task.title)
+    }
+    
+    func testConfigureSetsDate() {
+        let task = Task(title: "Foo")
+        cell.configure(withTask: task)
+        
+        let df = DateFormatter()
+        df.dateFormat = "dd.MM.yy"
+        let date = task.date
+        let dateString = df.string(from: date!)
+        
+        XCTAssertEqual(cell.dateLabel.text, dateString)
+    }
+    
+    func testConfigureSetsLocation() {
+        let location = Location(name: "Foo")
+        let task = Task(title: "Bar", location: location)
+        cell.configure(withTask: task)
+        
+        XCTAssertEqual(cell.locationLabel.text, task.location?.name)
+    }
+    
+    func configureCellWithTask() {
+        let task = Task(title: "Foo")
+        cell.configure(withTask: task, done: true)
+    }
+    
+    func testDoneTaskShouldStrikeThrough() {
+        configureCellWithTask()
+
+        let attributedString = NSAttributedString(string: "Foo", attributes: [NSAttributedString.Key.strikethroughStyle : NSUnderlineStyle.single.rawValue])
+
+        XCTAssertEqual(cell.titleLabel.attributedText, attributedString)
+    }
+    
+    func testDoneTaskDateLabelEqualsNil() {
+        configureCellWithTask()
+        
+        XCTAssertEqual(cell.dateLabel, nil)
+    }
+    
+    func testDoneTaskLocationLabelEqualsNil() {
+        configureCellWithTask()
+        
+        XCTAssertEqual(cell.locationLabel, nil)
+    }
+
+}
+
+extension TaskCellTests {
+    class FakeDataSource: NSObject, UITableViewDataSource {
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return 1
+        }
+        
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            return UITableViewCell()
+        }
+        
+        
+    }
+}
